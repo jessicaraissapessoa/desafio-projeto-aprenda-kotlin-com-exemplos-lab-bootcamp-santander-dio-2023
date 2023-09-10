88559fc5b6839424e19f99693e525f71a5dacbd4
jessicaraissapessoa <jessicaraissapessoa@gmail.com>
2023-09-10 01:47:20 -0300

    Esqueleto do projeto

--- src/main/kotlin/desafioAbstracaoFormacoesDIO.kt
+++ src/main/kotlin/desafioAbstracaoFormacoesDIO.kt
@@ -1,3 +1,28 @@
+/**
+ * @author Jéssica Raissa Pessoa
+ * Desafio de projeto - Bootcamp Santander 2023 - Mobile Android com Kotlin
+ * Tema: Abstração das Formações DIO
+ */
+
+//Classes:
+
+data class Usuario (var nome: String) //TODO
+
+enum class NivelDificuldade { BASICO, INTERMEDIARIO, AVANCADO } //TODO
+
+data class ConteudoEducacional (var nome: String, val duracao: Int = 1) //TODO
+
+data class Formacao (val nome: String, var conteudos: List<ConteudoEducacional>) { //TODO
+
+    val inscritos = mutableListOf<Usuario>()
+
+    fun matricular (usuario: Usuario) {
+        TODO("$usuario")
+    }
+
+}
+
+//Aplicação na main:
 fun main() {
-    println("Hello, world!")
+    TODO("Testes")
 }
