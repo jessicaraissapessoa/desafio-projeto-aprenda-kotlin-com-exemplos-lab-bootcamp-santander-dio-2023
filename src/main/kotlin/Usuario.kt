cd3b1d23c72623345e70176f09e5e5b3d2fb3c34
jessicaraissapessoa <jessicaraissapessoa@gmail.com>
2023-09-11 15:35:59 -0300

    ConteudoEducacional.kt / início de Usuario.kt

--- src/main/kotlin/Usuario.kt
+++ src/main/kotlin/Usuario.kt
@@ -1,6 +1,20 @@
 data class Usuario (var nome: String) {
+    //TODO: Exibição de usuários. Inserir 1 ou +. Encaminhar para menu inicial (ou loop para inserir mais?)
+    companion object { //Solicitando ao usuário os valores que vão preencher os argumentos dessa classe
 
-    companion object {
+        fun preencherNomeUsuario(): String? {
+
+            println("Insira o nome do usuário:")
+            val nomeUsuario = readlnOrNull()
+
+            if (nomeUsuario.isNullOrEmpty() || nomeUsuario.any { it.isDigit() }) {
+                println("-----Nome inválido!-----".uppercase())
+                preencherNomeUsuario()
+            }
+
+            return nomeUsuario
+
+        }
 
     }
 
