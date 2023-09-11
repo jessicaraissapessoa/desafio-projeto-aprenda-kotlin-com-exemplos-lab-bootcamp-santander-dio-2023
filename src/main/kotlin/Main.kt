db1245b5c34808505ba6b174eafac0c32e66b737
jessicaraissapessoa <jessicaraissapessoa@gmail.com>
2023-09-11 18:47:33 -0300

    att Main 11/09

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,7 +6,13 @@
 
 fun main() {
 
+
+    val usuario = Usuario.GerenciamentoUsuarios.cadastrarUsuario()
+    println(Usuario.GerenciamentoUsuarios)
+    println(Usuario.GerenciamentoUsuarios)
     val conteudo = ConteudoEducacional.cadastrarConteudoEducacional()
+    println(conteudo)
+
 
     //para imprimir enum -> NivelDificuldade.valueOf("INTERMEDIARIO")
 
