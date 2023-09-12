ab2c4e5da49475b28cb1aa69d00142704179af12
jessicaraissapessoa <jessicaraissapessoa@gmail.com>
2023-09-12 00:23:00 -0300

    novo arquivo Usuario.kt com correções de Usuariorascunho.kt (Antigo Usuario)

--- src/main/kotlin/Usuario.kt
+++ src/main/kotlin/Usuario.kt
@@ -1,81 +1,52 @@
-data class Usuario (var idUsuario: Int ,var nome: String, var tipoUsuario: TipoUsuario) {
-    //TODO: Inserir 1 ou +. Encaminhar para menu inicial (ou loop para inserir mais?).  Verificação se usuário já existe (ver POO /equals personalizado (compara id):)
 
-    class GerenciamentoUsuarios {
+class Usuario (var idUsuario: Int, var nomeUsuario: String, var tipoUsuario: String) {
 
-        var id = 0
-        val listaUsuarios : MutableSet<Usuario> = mutableSetOf() //Lista de todos os usuários
+    var id = 0
+    var listaUsuarios : MutableSet<Usuario> = mutableSetOf()
 
-        companion object { //Solicitando ao usuário os valores que vão preencher os argumentos dessa classe
+    constructor() : this(0, "","")
 
-            fun cadastrarUsuario(): Usuario {
+    fun cadastrarUsuario() {
 
-                fun preencherNomeUsuario(): String {
+        var tecladoNomeUsuario : String?
+        var tecladoTipoUsuario : String?
 
-                    println("Insira o nome do usuário:")
-                    val nomeUsuario = readlnOrNull()
+        do {
+            println("Insira nome completo do usuário:")
+            tecladoNomeUsuario = readlnOrNull()
 
-                    if (nomeUsuario.isNullOrEmpty() || nomeUsuario.any { it.isDigit() }) {
-                        println("-----Nome inválido!-----".uppercase())
-                        preencherNomeUsuario() // Recursivo da função em caso de resposta inválida
-                    }
+            if (tecladoNomeUsuario.isNullOrEmpty() || tecladoNomeUsuario.any { it.isDigit() }) {
+                println("-----Nome inválido!-----".uppercase())
+            }
+        } while (tecladoNomeUsuario.isNullOrEmpty() || tecladoNomeUsuario.any { it.isDigit() })
 
-                    return nomeUsuario.toString()
-                }
+        do {
+            println("Informe o tipo de usuário")
+            tecladoTipoUsuario = readlnOrNull()
 
-                fun definirTipoUsuario(): TipoUsuario {
-
-                    println("Defina o tipo de usuário de usuário informando o número correspondente ao tipo:" +
-                            "\n1 - Instrutor(a)\n2 - Aluno(a)")
-                    val selecaoTipoUsuario = readlnOrNull()
-
-                    return when (selecaoTipoUsuario) {
-                        "1" -> TipoUsuario.INSTRUTOR
-                        "2" -> TipoUsuario.ALUNO
-                        "" -> definirTipoUsuario() // Recursivo da função em caso de resposta inválida
-                        else -> {
-                            println("-----Seleção inválida!-----".uppercase())
-                            definirTipoUsuario() // Recursivo da função em caso de resposta inválida
-                        }
-                    }
-                }
-
-                /*TODO: Comparação com existentes fun
-                //Sobrescrita da função equals: não quer fazer comparação de todas as propriedades. Quer comparar apenar id
-        override fun equals (other: Any?) =
-            other is User && other.id == this.id //Novo usuário (other) = User? id de other = ir de usuário?
-                 */
-
-                //Preenchendo os argumentos de Usuario com os retornos das funções
-                val nomeUsuario = preencherNomeUsuario()
-                val tipoUsuario = definirTipoUsuario()
-
-                val gerenciador = GerenciamentoUsuarios() //Instanciando a classe GerenciamentoUsuario para poder gerenciar listaUsuarios
-                val novoUsuario = Usuario(gerenciador.id++, nomeUsuario, tipoUsuario) //Compondo novo usuário
-                gerenciador.listaUsuarios.add(novoUsuario) //Adicionando novo usuário à listaUsuarios
-
-                //Retornando o preenchimento dos argumentos
-                return novoUsuario
-
+            if (tecladoTipoUsuario.isNullOrEmpty()) {
+                println("-----Nome inválido!-----".uppercase())
             }
 
-        }
+        } while (tecladoTipoUsuario.isNullOrEmpty())
 
-        fun exibirUsuarios() {
-            listaUsuarios.sortedBy { id }
-        }
-        fun excluirUsuario() {
-            println("___ LISTA DE USUÁRIOS ---")
-            println(exibirUsuarios())
-            println("--------------------")
-            println("Informe o id do usuário que deseja excluir:")
-        }
+
+        cadastroUsuario(nomeUsuario, tipoUsuario)
 
     }
 
+    fun cadastroUsuario(nomeUsuario: String, tipoUsuario: String) {
 
+        var usuario = Usuario()
 
+        usuario.nomeUsuario = nomeUsuario
+        usuario.tipoUsuario = tipoUsuario
+        usuario.id++
 
+        listaUsuarios.add(usuario)
+
+        if (listaUsuarios.isNotEmpty()) println("xuxuzinho beleza")
+
+    }
 
 }
-
