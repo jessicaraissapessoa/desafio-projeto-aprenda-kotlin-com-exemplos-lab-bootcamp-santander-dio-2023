102f77cd9379a32277b374f390fd9d16deb05618
jessicaraissapessoa <jessicaraissapessoa@gmail.com>
2023-09-13 23:54:35 -0300

    Usuario CONCLUÍDO -> editarUsuario() e pequenas melhorias

--- src/main/kotlin/Usuario.kt
+++ src/main/kotlin/Usuario.kt
@@ -1,5 +1,6 @@
 var listaUsuarios : MutableList<Usuario> = mutableListOf() //Lista de usuários
 
+
 fun exibirUsuarios(): String { //Função para exibir lista de usuários (listaUsuarios)
 
     val builder = StringBuilder() //Usando StringBuilder para construir a string
@@ -12,20 +13,21 @@ fun exibirUsuarios(): String { //Função para exibir lista de usuários (listaU
     return builder.toString() //Retorna a exibição da lista em uma String
 }
 
+
 fun cadastrarUsuario() { //Função para cadastrar usuário
 
     var tecladoNomeUsuario : String? //Variável de nomeUsuario que será recebida
-    var tecladoTipoUsuario : String?
-    var selecaoTipoUsuario: String
+    var tecladoTipoUsuario : String? //Variável de seleção para opções na definição do tipo de usuário que será recebida
+    var selecaoTipoUsuario: String //Variável de tipoUsuario que será definido
 
-    do { //Repete execução enquanto tecladoNomeUsuario não receber um valor que não seja nulo, vazio ou com números
+    do { //Repete execução enquanto tecladoNomeUsuario não receber um valor que não seja nulo, vazio, sem letras ou com números
         println("Insira nome completo do usuário:")
         tecladoNomeUsuario = readlnOrNull() //Recebimento do valor pelo teclado
 
-        if (tecladoNomeUsuario.isNullOrEmpty() || tecladoNomeUsuario.any { it.isDigit() }) {
+        if (tecladoNomeUsuario.isNullOrEmpty() || !tecladoNomeUsuario.any { it.isLetter() } || tecladoNomeUsuario.any { it.isDigit() }) {
             println("-----Nome inválido!-----".uppercase())
         }
-    } while (tecladoNomeUsuario.isNullOrEmpty() || tecladoNomeUsuario.any { it.isDigit() })
+    } while (tecladoNomeUsuario.isNullOrEmpty() || !tecladoNomeUsuario.any { it.isLetter() }|| tecladoNomeUsuario.any { it.isDigit() })
 
     do { //Repete execução enquanto não recebe 1 ou 2
         println("Selecione o tipo do usuário informando o número correspondente:" +
@@ -59,23 +61,24 @@ fun cadastrarUsuario() { //Função para cadastrar usuário
 
 }
 
+
 fun excluirUsuario() { //Função para remover usuário
 
     println("----- Lista de usuários cadastrados\n".uppercase()  + exibirUsuarios())
 
-    //Variável opcoes recebe os valores de cada id de usuário de listaUsuarios
-    var opcoes = mutableListOf<String>()
+    //Variável opcoesValidas recebe os valores de cada id de usuário de listaUsuarios
+    val opcoes = mutableListOf<String>()
     for (usuario in listaUsuarios) {
-        var opcao = usuario.idUsuario
+        val opcao = usuario.idUsuario
         opcoes.add(opcao.toString())
     }
 
     var selecaoRemocaoUsuario : String? //Variável que receberá opção informada por teclado em do/while seguinte
 
-    do {  //Repete execução enquanto não recebe um valor correspondente a algum dos valores da variável opcoes
+    do { //Repete execução enquanto não recebe um valor correspondente a algum dos valores da variável opcoes
 
-        println("Informe o ID do usuário que deseja exlcuir:")
-        selecaoRemocaoUsuario = readlnOrNull()
+        println("Informe o ID do usuário que deseja excluir:")
+        selecaoRemocaoUsuario = readlnOrNull() //Recebimento do valor pelo teclado
 
         if (!opcoes.contains(selecaoRemocaoUsuario)) {
             println("-----Seleção inválida!-----".uppercase()) //Imprime em caso de não passar na validação
@@ -83,13 +86,110 @@ fun excluirUsuario() { //Função para remover usuário
 
     } while (!opcoes.contains(selecaoRemocaoUsuario))
 
+    val usuarioRemovido = listaUsuarios.removeAt(index = (selecaoRemocaoUsuario?.toInt()!! - 1)) //Remoção do usuário
 
-
-    val usuarioremovido = listaUsuarios.removeAt(index = (selecaoRemocaoUsuario?.toInt()!! - 1)) //Remoção do usuário
-
-    println("Remoção de usuário bem sucedida:\n$usuarioremovido\n") //Mensagem de feedback da remoção
+    println("Remoção de usuário bem sucedida:\n$usuarioRemovido\n") //Mensagem de feedback da remoção
 
 }
+
+
+fun editarUsuario() {
+
+    println("----- Lista de usuários cadastrados\n".uppercase()  + exibirUsuarios())
+
+    //Variável opcoes recebe os valores de cada id de usuário de listaUsuarios
+    val opcoes = mutableListOf<String>()
+    for (usuario in listaUsuarios) {
+        val opcao = usuario.idUsuario
+        opcoes.add(opcao.toString())
+    }
+
+    var selecaoEdicaoUsuario : String? //Variável que receberá opção informada por teclado em do/while seguinte
+    var usuarioParaEdicao : Usuario? = null
+
+    do { //Repete execução enquanto não recebe um valor correspondente a algum dos valores da variável opcoes
+
+        println("Informe o ID do usuário que deseja editar:")
+        selecaoEdicaoUsuario = readlnOrNull() //Recebimento do valor do ID informado pelo usuário
+
+        if (!opcoes.contains(selecaoEdicaoUsuario)) {
+            println("-----Seleção inválida!-----".uppercase()) //Imprime em caso de não passar na validação
+        }
+
+    } while (!opcoes.contains(selecaoEdicaoUsuario))
+
+    val index = selecaoEdicaoUsuario?.toInt()!! - 1 //Variável que recebe valor do que será o índice do elemento de listaUsuarios
+    usuarioParaEdicao = listaUsuarios[index] //Usuário a ser editado é o elemento listaUsuarios[<índice do elemento dentro do array>]
+
+    //Edição do nome do usuário
+    do { //Repete execução enquanto não recebe um valor correspondente a "s" ou "n"
+        println("Deseja editar o nome do usuário? Digite 's' para sim ou 'n' para não")
+        val respostaEditarNomeUsuario = readlnOrNull() //Recebimento do valor pelo teclado
+
+        if (respostaEditarNomeUsuario.isNullOrEmpty() || ((respostaEditarNomeUsuario != "s") && (respostaEditarNomeUsuario != "n"))) {
+            println("-----Seleção inválida!-----".uppercase()) //Imprime em caso de não passar na validação
+        } else {
+            var tecladoNovoNomeUsuario : String? //Variável de novoNomeUsuario que preencherá nomeUsuario após ediçao
+
+            if (respostaEditarNomeUsuario == "s") { //Se recebeu "s", segue para alteração do nome do usuário
+                do { //Repete execução enquanto tecladoNovoNomeUsuario não receber um valor que não seja nulo, vazio, sem letras ou com números
+                    println("Insira novo nome completo do usuário:")
+                    tecladoNovoNomeUsuario = readlnOrNull() //Recebimento do valor pelo teclado
+
+                    if (tecladoNovoNomeUsuario.isNullOrEmpty() || !tecladoNovoNomeUsuario.any { it.isLetter() } || tecladoNovoNomeUsuario.any { it.isDigit() }) {
+                        println("-----Nome inválido!-----".uppercase())
+                    }
+                } while (tecladoNovoNomeUsuario.isNullOrEmpty() || !tecladoNovoNomeUsuario.any { it.isLetter() } || tecladoNovoNomeUsuario.any { it.isDigit() })
+
+                usuarioParaEdicao.nomeUsuario = tecladoNovoNomeUsuario //Atribuição de novo nome ao usuário do ID informado
+
+                println("Alteração do nome do usuário bem sucedida:\n$usuarioParaEdicao\n") //Mensagem de feedback da edição do nome do usuário
+            }
+        }
+    } while (respostaEditarNomeUsuario.isNullOrEmpty() || ((respostaEditarNomeUsuario != "s") && (respostaEditarNomeUsuario != "n")))
+
+    //Edição do tipo do usuário
+    do {  //Repete execução enquanto não recebe um valor correspondente a "s" ou "n"
+
+        println("Deseja editar o tipo do usuário? Digite 's' para sim ou 'n' para não")
+        val respostaEditarTipoUsuario = readlnOrNull() //Recebimento do valor pelo teclado
+
+        if (respostaEditarTipoUsuario.isNullOrEmpty() || ((respostaEditarTipoUsuario != "s") && (respostaEditarTipoUsuario != "n"))) {
+            println("-----Seleção inválida!-----".uppercase()) //Imprime em caso de não passar na validação
+        } else {
+            var selecaoNovoTipoUsuario : String //Variável de novoNomeUsuario que preencherá nomeUsuario após ediçao
+
+            if (respostaEditarTipoUsuario == "s") { //Se recebeu "s", segue para alteração do tipo do usuário
+                var selecaoNovoTipoUsuario : String?
+
+                do { //Repete execução enquanto não recebe 1 ou 2
+                    println("Selecione o novo tipo do usuário informando o número correspondente:" +
+                            "\n1 - Instrutor(a)\n2 - Aluno(a)")
+                    var tecladoNovoTipoUsuario = readlnOrNull() //Recebe escolha do usuário entre as opções
+                    selecaoNovoTipoUsuario = tecladoNovoTipoUsuario.toString() //recebe o valor de tecladoTipoUsuario
+
+
+                    when (tecladoNovoTipoUsuario) {
+                        "1" -> selecaoNovoTipoUsuario = TipoUsuario.INSTRUTOR.toString() //muda o valor de selecaoNovoTipoUsuario para o valor do enum TipoUsuario
+                        "2" -> selecaoNovoTipoUsuario = TipoUsuario.ALUNO.toString() //muda o valor de selecaoNovoTipoUsuario para o valor do enum TipoUsuario
+                    }
+
+                    if (tecladoNovoTipoUsuario.isNullOrEmpty() || (!tecladoNovoTipoUsuario.equals("1") && !tecladoNovoTipoUsuario.equals("2"))) {
+                        println("-----Seleção inválida!-----".uppercase()) //Imprime em caso de não passar na validação
+                    }
+
+                } while (tecladoNovoTipoUsuario.isNullOrEmpty() || (!tecladoNovoTipoUsuario.equals("1") && !tecladoNovoTipoUsuario.equals("2")))
+
+                usuarioParaEdicao.tipoUsuario = selecaoNovoTipoUsuario!! //Alteração do tipo do usuário do ID informado
+
+                println("Alteração do tipo do usuário bem sucedida:\n$usuarioParaEdicao\n") //Mensagem de feedback da edição do tipo do usuário
+            }
+        }
+    } while (respostaEditarTipoUsuario.isNullOrEmpty() || ((respostaEditarTipoUsuario != "s") && (respostaEditarTipoUsuario != "n")))
+
+    println("Edição do usuário de ID $selecaoEdicaoUsuario concluída") //Feedback da conclusão da edição do usuário
+}
+
 
 data class Usuario (var idUsuario: Int, var nomeUsuario: String, var tipoUsuario: String) { //Classe Usuario
 
