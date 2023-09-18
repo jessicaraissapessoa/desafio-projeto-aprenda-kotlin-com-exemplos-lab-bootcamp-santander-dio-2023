681f5914ae769143fda85fe81c6c21cb6923bc9c
jessicaraissapessoa <jessicaraissapessoa@gmail.com>
2023-09-17 23:37:19 -0300

    Testes do que foi implementado anteriormente em Formacao

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,10 +6,123 @@
 
 fun main() {
 
+
+
+
+
+
     cadastrarUsuario()
+
+    println("\nSAINDO DE CADASTRARUSUARIO E INDO PARA CADASTRARCONTEUDOEDUCACIONAL\n")
+
+    cadastrarConteudoEducacional()
+
+    println("\nSAINDO DE CADASTRARCONTEUDOEDUCACIONAL E INDO PARA CADASTRARFORMACAO\n")
+
+    cadastrarFormacao()
+
+    println("\nSAINDO DE CADASTRARFORMACAO E INDO PARA CADASTRARUSUARIOFORMACAO\n")
+
+    cadastrarUsuarioFormacao()
+
+    println("\nSAINDO DE CADASTRARUSUARIOFORMACAO E INDO PARA EXIBIR CADASTRARCONTEUDOFORMACAO\n")
+
+    cadastrarConteudoFormacao()
+
+    println("\nSAINDO DE CADASTRARCONTEUDOFORMACAO E INDO PARA EXIBIR EXIBIRFORMACAODETALHADA\n")
+
+    exibirFormacaoDetalhada()
+
+    println("\nSAINDO DE FORMACAODETALHADA E INDO PARA EXIBIRLISTAFORMACOES\n")
+
+
+    exibirListaFormacoes()
+
+    println("\nSAINDO DE EXIBIRLISTAFORMACOES\n")
+
+
+
+    println("\n\n\n")
+
+    //var formacao = Formacao(1, "a", )
+    //var teste = ConteudoEducacional(1, "a", "a",  "a",  1)
+    //formacao.conteudosFormacao
+    //println(formacao)
+
     cadastrarUsuario()
-    println(exibirUsuarios())
+    cadastrarConteudoEducacional()
+    cadastrarFormacao()
+    exibirListaFormacoes()
+
+
+
+    println("\n\n\n\n\n")
+
+    cadastrarConteudoEducacional()
+    editarConteudoEducacional()
+    excluirConteudoEducacional()
+
+    exibirUsuarios()
+
+    cadastrarUsuario()
+    //cadastrarUsuario()
+    println("---------------------------------------------------------")
+
+    exibirUsuarios()
+    //println("---------------------------------------------------------")
+    editarUsuario()
     //excluirUsuario()
+    println("---------------------------------------------------------")
+
+
+    //cadastrarConteudoEducacional()
+    //var teste = ConteudoEducacional(1, "a", "a",  "a",  1)
+
+    //println(listaConteudosEducacionais.add(teste))
+    println(listaConteudosEducacionais)
+
+    println("\n\n")
+
+    exibirConteudosEducacionais()
+    //println(listaConteudosEducacionais)
+    //exibirConteudosEducacionais()
+
+//    exibirConteudosEducacionais()
+//    println("-----------------------------------------------------------------------------------------------------")
+//    println("de exibir para cadastrar")
+//    println("-----------------------------------------------------------------------------------------------------")
+//    cadastrarConteudoEducacional()
+//    println("-----------------------------------------------------------------------------------------------------")
+//    println("de cadastrar para exibir")
+//    println("-----------------------------------------------------------------------------------------------------")
+//    exibirConteudosEducacionais()
+//    println("-----------------------------------------------------------------------------------------------------")
+//    println("de exibir para editar")
+//    println("-----------------------------------------------------------------------------------------------------")
+//    editarConteudoEducacional()
+//    println("-----------------------------------------------------------------------------------------------------")
+//    println("de editar para exibir")
+//    println("-----------------------------------------------------------------------------------------------------")
+//    exibirConteudosEducacionais()
+//    println("-----------------------------------------------------------------------------------------------------")
+//    println("de exibir para excluir")
+//    println("-----------------------------------------------------------------------------------------------------")
+//    excluirConteudoEducacional()
+//    println("-----------------------------------------------------------------------------------------------------")
+//    println("de excluir para exibir")
+//    println("-----------------------------------------------------------------------------------------------------")
+//    exibirConteudosEducacionais()
+//    println("-----------------------------------------------------------------------------------------------------")
+//    println("-----------------------------------------------------------------------------------------------------")
+
+
+
+
+    //println(exibirUsuarios())
+    //var teste = ConteudoEducacional(1, "a", "a",  "a",  1)
+    //println(teste)
+
+
 
     //println("---")
     //println(exibirUsuarios())
