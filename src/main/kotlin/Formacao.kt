73fdd90b90b07cf175d50d13ec0b310a0ff744c4
jessicaraissapessoa <jessicaraissapessoa@gmail.com>
2023-09-18 21:23:12 -0300

    exibirInscritosFormacao() e exibirConteudosFormacao()

--- src/main/kotlin/Formacao.kt
+++ src/main/kotlin/Formacao.kt
@@ -109,6 +109,31 @@ fun exibirFormacaoDetalhada() {
 }
 
 
+fun exibirInscritosFormacao(formacaoSelecionada: Formacao) {
+
+    val builder = StringBuilder()
+
+    for (usuario in formacaoSelecionada.inscritosFormacao) {
+        builder.append(usuario.toString())
+        builder.append("\n") //Adicionar uma quebra de linha entre cada usuário inscrito na formação exibido
+    }
+
+    println(builder.toString())
+}
+
+fun exibirConteudosFormacao(formacaoSelecionada: Formacao) {
+
+    val builder = StringBuilder()
+
+    for (conteudo in formacaoSelecionada.conteudosFormacao) {
+        builder.append(conteudo.toString())
+        builder.append("\n") //Adicionar uma quebra de linha entre cada conteúdo cadastrado na formação exibido
+    }
+
+    println(builder.toString())
+}
+
+
 fun Formacao.equalsIgnoringID(other: Formacao) : Boolean {
     return nomeFormacao == other.nomeFormacao && nivelDificuldadeFormacao == other.nivelDificuldadeFormacao
 }
@@ -601,14 +626,92 @@ fun editarFormacao() {
 
 fun editarDadosFormacao(formacaoSelecionada: Formacao) {
 
+    println("ID: ${formacaoSelecionada.idFormacao} | NOME: ${formacaoSelecionada.nomeFormacao}\n↳ NÍVEL: ${formacaoSelecionada.nivelDificuldadeFormacao}")
+
+    //Edição do nome da formação
+    do { //Repete execução enquanto não recebe um valor correspondente a "s" ou "n"
+        println("Deseja editar o nome da formação? Digite 's' para sim ou 'n' para não")
+        val respostaEditarNomeFormacao = readlnOrNull() //Recebimento do valor pelo teclado
+
+        if (respostaEditarNomeFormacao.isNullOrEmpty() || ((respostaEditarNomeFormacao != "s") && (respostaEditarNomeFormacao != "n"))) {
+            println("-----Seleção inválida!-----".uppercase()) //Imprime em caso de não passar na validação
+        } else {
+            var tecladoNovoNomeFormacao : String? //Variável de novoNomeFormacao que preencherá nomeFormacao após edição
+
+            if (respostaEditarNomeFormacao == "s") { //Se recebeu "s", segue para alteração do nome da formação
+                do { //Repete execução enquanto tecladoNovoNomeFormacao não receber um valor que não seja nulo, vazio, sem letras ou com números
+                    println("Insira novo nome completo do usuário:")
+                    tecladoNovoNomeFormacao = readlnOrNull() //Recebimento do valor pelo teclado
+
+                    if (tecladoNovoNomeFormacao.isNullOrEmpty() || !tecladoNovoNomeFormacao.any { it.isLetter() } || tecladoNovoNomeFormacao.any { it.isDigit() }) {
+                        println("-----Nome inválido!-----".uppercase())
+                    }
+                } while (tecladoNovoNomeFormacao.isNullOrEmpty() || !tecladoNovoNomeFormacao.any { it.isLetter() } || tecladoNovoNomeFormacao.any { it.isDigit() })
+
+                formacaoSelecionada.nomeFormacao = tecladoNovoNomeFormacao //Atribuição de novo nome à formação do ID informado
+
+                //Mensagem de feedback da edição do nome da formação
+                println("Alteração do nome da formação bem sucedida:\n" +
+                        "ID: ${formacaoSelecionada.idFormacao} | NOME: ${formacaoSelecionada.nomeFormacao}\n\t↳ NÍVEL: ${formacaoSelecionada.nivelDificuldadeFormacao}\n")
+            }
+        }
+    } while (respostaEditarNomeFormacao.isNullOrEmpty() || ((respostaEditarNomeFormacao != "s") && (respostaEditarNomeFormacao != "n")))
+
+    //Edição do nível de dificuldade da formação
+    do {  //Repete execução enquanto não recebe um valor correspondente a "s" ou "n"
+
+        println("Deseja editar o nível de dificuldade da formação? Digite 's' para sim ou 'n' para não")
+        val respostaEditarNivelDificuldadeFormacao = readlnOrNull() //Recebimento do valor pelo teclado
+
+        if (respostaEditarNivelDificuldadeFormacao.isNullOrEmpty() || ((respostaEditarNivelDificuldadeFormacao != "s") && (respostaEditarNivelDificuldadeFormacao != "n"))) {
+            println("-----Seleção inválida!-----".uppercase()) //Imprime em caso de não passar na validação
+        } else {
+            var selecaoNovoNivelDificuldadeFormacao : String //Variável de novoNivelDificuldadeFormacao que preencherá NivelDificuldadeFormacao após edição
+
+            if (respostaEditarNivelDificuldadeFormacao == "s") { //Se recebeu "s", segue para alteração do nível de dificuldade da formação
+
+                do { //Repete execução enquanto não recebe 1, 2 ou 3
+                    println("\"Selecione o novo nível de dificuldade da formação informando o número correspondente:" +
+                            "\n1 - Básico\n2 - Intermediário\n3 - Avançado")
+                    var tecladoNivelDificuldadeFormacao = readlnOrNull() //Recebe escolha do usuário entre as opções
+                    selecaoNovoNivelDificuldadeFormacao = tecladoNivelDificuldadeFormacao.toString() //recebe o valor de tecladoNivelDificuldadeFormacao
+
+
+                    when (tecladoNivelDificuldadeFormacao) {
+                        "1" -> selecaoNovoNivelDificuldadeFormacao = NivelDificuldade.BASICO.toString() //muda o valor de selecaoNovoNivelDificuldadeFormacao para o valor do enum NivelDificuldade
+                        "2" -> selecaoNovoNivelDificuldadeFormacao = NivelDificuldade.INTERMEDIARIO.toString()  //muda o valor de selecaoNovoNivelDificuldadeFormacao para o valor do enum NivelDificuldade
+                        "3" -> selecaoNovoNivelDificuldadeFormacao = NivelDificuldade.AVANCADO.toString() //muda o valor de selecaoNovoNivelDificuldadeFormacao para o valor do enum NivelDificuldade
+
+                    }
+
+                    if (tecladoNivelDificuldadeFormacao.isNullOrEmpty() || (!tecladoNivelDificuldadeFormacao.equals("1") && !tecladoNivelDificuldadeFormacao.equals("2") && !tecladoNivelDificuldadeFormacao.equals("3"))) {
+                        println("-----Seleção inválida!-----".uppercase()) //Imprime em caso de não passar na validação
+                    }
+
+                } while (tecladoNivelDificuldadeFormacao.isNullOrEmpty() || (!tecladoNivelDificuldadeFormacao.equals("1") && !tecladoNivelDificuldadeFormacao.equals("2") && !tecladoNivelDificuldadeFormacao.equals("3")))
+
+                formacaoSelecionada.nivelDificuldadeFormacao = selecaoNovoNivelDificuldadeFormacao //Alteração do nível de dificuldade da formação do ID informado
+
+                //Mensagem de feedback da edição do nível de dificuldade da formação
+                println("Alteração do tipo do conteúdo educacional bem sucedida:\n" +
+                        "ID: ${formacaoSelecionada.idFormacao} | NOME: ${formacaoSelecionada.nomeFormacao}\n\t↳ NÍVEL: ${formacaoSelecionada.nivelDificuldadeFormacao}\n")
+            }
+        }
+    } while (respostaEditarNivelDificuldadeFormacao.isNullOrEmpty() || ((respostaEditarNivelDificuldadeFormacao != "s") && (respostaEditarNivelDificuldadeFormacao != "n")))
+
 }
 
 
 fun editarUsuariosFormacao(formacaoSelecionada: Formacao) {
 
+    exibirInscritosFormacao(formacaoSelecionada)
+    println("fim")
+
 }
 
 
 fun editarConteudosFormacao(formacaoSelecionada: Formacao) {
+
+
 
 }
