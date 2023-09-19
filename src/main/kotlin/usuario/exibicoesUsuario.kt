c5107f00efc7f1b78c69e261860e519dcce69994
jessicaraissapessoa <jessicaraissapessoa@gmail.com>
2023-09-19 18:46:10 -0300

    exibirUsuariosOpcaoMenu()

--- src/main/kotlin/usuario/exibicoesUsuario.kt
+++ src/main/kotlin/usuario/exibicoesUsuario.kt
@@ -1,5 +1,7 @@
 package usuario
 
+import aplicacao.menuUsuario
+
 fun exibirListaUsuariosVazia() {
 
     do { //Repete execução enquanto desejaAdicionarUsuarioTeclado não receber um valor que não seja nulo, vazio, sem letras ou diferente de "s" e "n"
@@ -33,4 +35,32 @@ fun exibirUsuarios() {
     }
 
     println(builder.toString())
+}
+
+
+fun exibirUsuariosOpcaoMenu() { //Versão de exibirUsuarios() para encaminhamento de menuUsuario()
+
+    if (listaUsuarios.isEmpty()) exibirListaUsuariosVazia() //Caso a lista de usuários esteja vazia, executar função exibirListaUsuariosVazia()
+
+    val builder = StringBuilder() //Usando StringBuilder para construir a string
+
+    for (usuario in listaUsuarios) {
+        builder.append(usuario.toString()) //Adiciona o toString() de cada usuário
+        builder.append("\n") //Adicionar uma quebra de linha entre cada usuário
+    }
+
+    println(builder.toString())
+
+    do { //Repete execução enquanto editarOutroUsuario não receber um valor que não seja nulo, vazio, sem letras ou diferente de "s"
+
+        println("Digite 's' para voltar ao menu de USUÁRIO")
+        val voltarParaMenuUsuario = readlnOrNull() //Recebimento do valor pelo teclado
+
+        when(voltarParaMenuUsuario) {
+            "s" -> menuUsuario() //Segue para função menuUsuario()
+            "n" -> print("")
+        }
+
+    } while (voltarParaMenuUsuario.isNullOrEmpty() || !voltarParaMenuUsuario.any { it.isLetter() } || (!voltarParaMenuUsuario.equals("s")))
+
 }
